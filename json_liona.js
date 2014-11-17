ad78ce9525f7f5e5912a53a2b14128b8a5c8caf1
Mark Tomm <marktomm90@gmail.com>
2014-11-18 00:49:12 +0200

    initial commit

---
+++ json_liona.js
@@ -0,0 +1,84 @@
+var hotelsImgLibCont = {
+   "/hungary" : {
+      "/balaton" : {
+	"/2008-hotel-azur-img_b.jpg" : 1,
+         "/club-imola" : {
+            "/11485-club-imola-hotel-img_b.jpg" : 1,
+            "/11482-club-imola-hotel-img_b.jpg" : 1,
+            "/11486-club-imola-hotel-img_b.jpg" : 1,
+            "/11483-club-imola-hotel-img_b.jpg" : 1
+         },
+         "/azur" : {
+            "/club-imola" : {
+                "/11485-club-imola-hotel-img_b.jpg" : 1,
+                "/11482-club-imola-hotel-img_b.jpg" : 1,
+                "/11486-club-imola-hotel-img_b.jpg" : 1,
+                "/11483-club-imola-hotel-img_b.jpg" : 1
+            },
+            "/2008-hotel-azur-img_b.jpg" : 1,
+            "/2005-hotel-azur-img_b.jpg" : 1,
+            "/2000-hotel-azur-img_b.jpg" : 1,
+            "/2006-hotel-azur-img_b.jpg" : 1,
+            "/2009-hotel-azur-img_b.jpg" : 1,
+            "/2004-hotel-azur-img_b.jpg" : 1
+         },
+         "/uni" : {
+            "/shop-items-catalog-image118009.jpg" : 1,
+            "/1.jpg" : 1,
+            "/4921173.jpg" : 1,
+            "/69537b.jpg" : 1,
+            "/69541b.jpg" : 1,
+            "/109.jpg" : 1
+         },
+         "/astoria" : {
+            "/11514-hotel-astoria-img_b.jpg" : 1,
+            "/11511-hotel-astoria-img_b.jpg" : 1,
+            "/11513-hotel-astoria-img_b.jpg" : 1,
+            "/11512-hotel-astoria-img_b.jpg" : 1,
+            "/1998-hotel-astoria-img_b.jpg" : 1,
+            "/1993-hotel-astoria-img_b.jpg" : 1,
+            "/11510-hotel-astoria-img_b.jpg" : 1,
+            "/1996-hotel-astoria-img_b.jpg" : 1
+         },
+         "/aranypart" : {
+            "/11502-Hotel Aranypart-img_b.jpg" : 1,
+            "/11509-Hotel Aranypart-img_b.jpg" : 1,
+            "/11506-Hotel Aranypart-img_b.jpg" : 1,
+            "/11503-Hotel Aranypart-img_b.jpg" : 1,
+            "/11500-Hotel Aranypart-img_b.jpg" : 1,
+            "/11508-Hotel Aranypart-img_b.jpg" : 1,
+            "/11507-Hotel Aranypart-img_b.jpg" : 1,
+            "/11505-Hotel Aranypart-img_b.jpg" : 1,
+            "/11501-Hotel Aranypart-img_b.jpg" : 1,
+            "/11504-Hotel Aranypart-img_b.jpg" : 1,
+            "/11499-Hotel Aranypart-img_b.jpg" : 1
+         },
+         "/anna-grand" : {
+            "/11476-anna-grand-img_b.jpg" : 1,
+            "/11477-anna-grand-img_b.jpg" : 1,
+            "/11472-anna-grand-img_b.jpg" : 1,
+            "/11474-anna-grand-img_b.jpg" : 1,
+            "/11473-anna-grand-img_b.jpg" : 1,
+            "/11471-anna-grand-img_b.jpg" : 1,
+            "/11475-anna-grand-img_b.jpg" : 1,
+            "/11478-anna-grand-img_b.jpg" : 1,
+            "/11470-anna-grand-img_b.jpg" : 1,
+            "/11481-anna-grand-img_b.jpg" : 1
+         },
+         "/annabella" : {
+            "/11498-hotel-annabella-img_b.jpg" : 1,
+            "/11496-hotel-annabella-img_b.jpg" : 1,
+            "/1984-Annabella-img_b.jpg" : 1,
+            "/11494-hotel-annabella-img_b.jpg" : 1,
+            "/1988-Annabella-img_b.jpg" : 1,
+            "/1992-Annabella-img_b.jpg" : 1,
+            "/1990-Annabella-img_b.jpg" : 1,
+            "/11493-hotel-annabella-img_b.jpg" : 1,
+            "/1986-Annabella-img_b.jpg" : 1,
+            "/1989-Annabella-img_b.jpg" : 1,
+            "/11495-hotel-annabella-img_b.jpg" : 1,
+            "/1985-Annabella-img_b.jpg" : 1
+         }
+      }
+   }
+}
